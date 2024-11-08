fb49b8a1ce6010db351fe04403c5929a1c1beaa5
dilli <dilliraj.116@gmail.com>
2024-11-08 08:26:41 -0800

    Tf script for linux vm resource

---
+++ TF_Task/TF_VM_linux/resgrp.tf
@@ -0,0 +1,72 @@
+resource "azurerm_resource_group" "rg" {
+  location = "westus2"
+  name     = "tf_rg"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet1"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+}
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_ssh_public_key" "ssh" {
+  name                = "ssh_key"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  public_key          = file("/home/dilli/.ssh/id_rsa.pub")
+
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pub1.id
+  }
+}
+resource "azurerm_public_ip" "pub1" {
+  name                = "pub1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+resource "azurerm_linux_virtual_machine" "linvm" {
+  name                = "linuxvm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = azurerm_ssh_public_key.ssh.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
